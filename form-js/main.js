7005bb6071b020aa393c38786d4391ef32a95e2b
Amasías Rodríguez <100006082+Amasias-Rodriguez@users.noreply.github.com>
2024-01-04 22:57:23 -0600

    Update main.js

--- form-js/main.js
+++ form-js/main.js
@@ -4,12 +4,26 @@ function validarFormulario() {
 
     // Validar que las contraseñas coincidan
     if (contrasena !== confirmPassword) {
-        alert("Las contraseñas no coinciden");
+        mostrarAlertaRoja("Las contraseñas no coinciden");
         return false; // Evita el envío del formulario
     }
 
-
     // Si todo está bien, puedes enviar el formulario
-    alert("¡Formulario enviado con éxito!");
+    mostrarAlertaRoja("¡Formulario enviado con éxito!", false);
     return true;
+}
+
+function mostrarAlertaRoja(mensaje, error = true) {
+    // Crear una alerta personalizada con color rojo o verde
+    var alerta = document.createElement("div");
+    alerta.innerHTML = mensaje;
+    alerta.className = error ? "error-alert" : "success-alert";
+
+    // Agregar la alerta al cuerpo del documento
+    document.body.appendChild(alerta);
+
+    // Después de unos segundos, eliminar la alerta
+    setTimeout(function() {
+        alerta.remove();
+    }, 3000); // 3000 milisegundos = 3 segundos
 }
